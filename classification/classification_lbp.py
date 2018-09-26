501c624f36929b2fe32f8ac33c241fc0425d00f4
Thales C. Vescovi <thalescarreta@gmail.com>
2018-09-26 00:58:08 -0300

    KNN treinando LBP 'ok'

--- classification/classification_lbp.py
+++ classification/classification_lbp.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import cv2
 import algorithms.lbp as lbp
@@ -10,7 +12,7 @@ def leitura_arquivos(arq):
     # Realiza dump de dados dos histogramas para listas in memory
     lstHist = []
     strLinhaArq = arq.readline()
-    while (strLinhaArq != ''):
+    while (strLinhaArq != ''):                                         # remover '#'
         lstId = strLinhaArq.split(';')
         histograma = ast.literal_eval(lstId[2])
         lstHist.append([lstId[0], lstId[1], histograma])
@@ -19,24 +21,65 @@ def leitura_arquivos(arq):
     return lstHist
 
 
+def separa_itens_treinamento(lstArqHistSuj):
+    lstHist = []
+    lstSujeitoAux = []
+    lstSujeitos = []
+    for row in range(0, len(lstArqHistSuj)):
+        lstHist.append(lstArqHistSuj[row][2])
+        lstSujeitoAux.append(int(lstArqHistSuj[row][0]))
+        lstSujeitos.append(lstSujeitoAux)
+        lstSujeitoAux = []
+    #end for
+    return np.array(lstHist, dtype=np.float32), np.array(lstSujeitos, dtype=np.float32)
+
 
 
 def main():
     arqHistSujeitos = open("C:\\Users\\thale\\PycharmProjects\\VaiMeuFilho\\bd_projeto\\histograms\\base_histogramas_lbp.txt", "r")
     arqHistTestes = open("C:\\Users\\thale\\PycharmProjects\\VaiMeuFilho\\bd_projeto\\histograms\\teste_histogramas_lbp.txt", "r")
-
     # Estrutura: [ [s, f, [h,i,s,t,o,g,r,a,m,a]], [s, f, [h,i,s,t,o,g,r,a,m,a]], [s, f, [h,i,s,t,o,g,r,a,m,a]] ]
-    lstHistTestes = leitura_arquivos(arqHistSujeitos)
-    lstHistSujeitos = leitura_arquivos(arqHistTestes)
+    lstArqHistSujeitos = leitura_arquivos(arqHistSujeitos)
+    lstArqHistTestes = leitura_arquivos(arqHistTestes)
+
+
+    # TREINAMENTO
+    lstHist, lstSujeitos = separa_itens_treinamento(lstArqHistSujeitos)
+    print(lstHist)
+    #print(lstSujeitos)
+    knn = cv2.ml.KNearest_create()
+    knn.train(lstHist, cv2.ml.ROW_SAMPLE, lstSujeitos)   # knn.train(trainData, responses)
+
+
+    #CLASSIFICAÇÃO
+    #testeSet = random.choice(lstArqHistTestes)
+    testeSet = np.array(lstArqHistTestes[14][2], dtype=np.float32)  # Em Teste: Sujeito 14; face 4;
+    print(testeSet)
+
+    ret, results, neighbours, dist = knn.findNearest(testeSet, 3)
+
+    # print("ret: ", ret, "\n")
+    # print("result: ", results, "\n")
+    # print("neighbours: ", neighbours, "\n")
+    # print("distance: ", dist)
+
+
+    # return results.reshape(1, len(results))[0].astype(int)
+
+
+    #arqHistTestes = open("C:\\Users\\thale\\PycharmProjects\\VaiMeuFilho\\code_old\\teste.txt", "w")
+    #arqHistTestes.write(str(lstHist[0]))
+    #arqHistTestes.close()
+
+
+
+
+
+
 
     arqHistSujeitos.close()
     arqHistTestes.close()
 
 
-
-
-
-
-
 if __name__ == '__main__':
-	main()
+    main()
--- code_old/testes.py
+++ code_old/testes.py
@@ -1,22 +1,32 @@
 import numpy as np
+import cv2
+import algorithms.lbp as lbp
+import sys
 import ast
-
+import algorithms.lbp as lbp
 
 def main():
-    #chaveSujeitoTeste = np.random.randint(0, 2)
-    #print(chaveSujeitoTeste)
+    # Feature set containing (x,y) values of 25 known/training data
+    trainData = np.random.randint(0, 100, (25, 2)).astype(np.float32)
 
+    # Labels each one either Red or Blue with numbers 0 and 1
+    responses = np.random.randint(0, 2, (25, 1)).astype(np.float32)
 
-    string = '[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]'
-    print(string)
-    lst = ast.literal_eval(string)
-    print(lst[2])
+    print(trainData)
+    print(responses)
 
+    knn = cv2.ml.KNearest_create()
+    knn.train(trainData, cv2.ml.ROW_SAMPLE, responses)
 
+    newcomer = np.random.randint(0, 100, (1, 2)).astype(np.float32)
+    print(newcomer)
 
+    ret, results, neighbours, dist = knn.findNearest(newcomer, 3)
 
-
-
+    print("ret: ", ret, "\n")
+    print("result: ", results, "\n")
+    print("neighbours: ", neighbours, "\n")
+    print("distance: ", dist)
 
 if __name__ == '__main__':
 	main()
